cd50d5ccae8329e7f3c96ecd10bde25dcc972986
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2023-04-20 14:06:08 +0530

    'created registration form using django form'

--- website/views.py
+++ website/views.py
@@ -1,33 +1,55 @@
+from wsgiref.util import request_uri
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 
 
 def home(request):
     # check to see if logging in
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
 
         # authenticate
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, "You Have been logged in.")
-            return redirect('home')
+            return redirect("home")
         else:
-            messages.error(request, 'There was an error!')
-            return redirect('home')
+            messages.error(request, "There was an error!")
+            return redirect("home")
     else:
-        return render(request, 'home.html', {})
+        return render(request, "home.html", {})
 
 
 def login_user(request):
     pass
 
 
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect("home")
+    else:
+        form = SignUpForm()
+        return render(request, "register.html", {"form": form})
+
+    return render(request, "register.html", {"form": form})
+
+
 def logout_user(request):
     logout(request)
-    messages.success(request,"Logged Ot..")
+    messages.success(request, "Logged Out..")
+    return redirect("home")
